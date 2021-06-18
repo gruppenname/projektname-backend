4f36422904a0001c359d729b0b9a453a5d9993f0
katharina <katharina.sachs@seerene.com>
2021-06-18 23:11:04 +0200

    docker compose with FE

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import todoRouter from './routes/todos/todos';
 import debug from 'debug';
 
+const cors = require('cors');
 const log = debug('server');
-const app = express();
+let app = express();
 
 app.disable('x-powered-by');  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use('/todos', todoRouter)
 
